02e287005601664463c39f827c7325815960a40f
Colin <harebovmakar@yandex.ru>
2023-01-15 18:55:09 +0500

    New Chages!

---
+++ __init__.py
@@ -0,0 +1,39 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import pandas as pd
+import os
+
+class parser:
+    def main(self):
+        print("""
+░██████╗████████╗░█████╗░░█████╗░██╗░░██╗░█████╗░██╗░░░██╗███████╗██████╗░███████╗██╗░░░░░░█████╗░░██╗░░░░░░░██╗
+██╔════╝╚══██╔══╝██╔══██╗██╔══██╗██║░██╔╝██╔══██╗██║░░░██║██╔════╝██╔══██╗██╔════╝██║░░░░░██╔══██╗░██║░░██╗░░██║
+╚█████╗░░░░██║░░░███████║██║░░╚═╝█████═╝░██║░░██║╚██╗░██╔╝█████╗░░██████╔╝█████╗░░██║░░░░░██║░░██║░╚██╗████╗██╔╝
+░╚═══██╗░░░██║░░░██╔══██║██║░░██╗██╔═██╗░██║░░██║░╚████╔╝░██╔══╝░░██╔══██╗██╔══╝░░██║░░░░░██║░░██║░░████╔═████║░
+██████╔╝░░░██║░░░██║░░██║╚█████╔╝██║░╚██╗╚█████╔╝░░╚██╔╝░░███████╗██║░░██║██║░░░░░███████╗╚█████╔╝░░╚██╔╝░╚██╔╝░
+╚═════╝░░░░╚═╝░░░╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝░╚════╝░░░░╚═╝░░░╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚══════╝░╚════╝░░░░╚═╝░░░╚═╝░░""")
+        print("File Name")
+        name_file = input()
+        file = open(f'{name_file}.html', 'w', encoding='utf-8')
+        print("Your User ID on Stackoverflow:")
+        url = input("") # replace with the URL of the website you want to scrape
+        print("Your Name on Stackoverflow")
+        url_name = str.lower(input())
+        url2 = f"https://ru.stackoverflow.com/users/{url}/{url_name}?tab=profile"
+
+        # Make a request to the website
+        
+        response = requests.get(url2)
+
+        # Parse the HTML content
+        soup = bs(response.content, "html.parser")
+
+        # Find the elements with the class "d-flex flex__allitems6 gs16 fw-wrap md:jc-space-between"
+        data = soup.find_all("div", class_="d-flex flex__allitems6 gs16 fw-wrap md:jc-space-between")
+
+        # Print the data, you can use it for whatever you want
+
+        saved = file.write(str(data))
+        print(f"File Saved to {name_file}.html")
+        file.close
+        input("Press Enter to exit")
---
+++ main.py
@@ -0,0 +1,3 @@
+import __init__
+parser = __init__
+parser.parser.main(self=print())
