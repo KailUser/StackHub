dbbae4b542229898463273a754d92ef4eb22a487
Colin <harebovmakar@yandex.ru>
2023-01-15 21:06:16 +0500

    Update to 1.5v

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
-import __init__
-parser = __init__
-parser.parser.main(self=print())
+import __init__ as pd
+
+print("""
+░██████╗████████╗░█████╗░░█████╗░██╗░░██╗██╗░░██╗██╗░░░██╗██████╗░
+██╔════╝╚══██╔══╝██╔══██╗██╔══██╗██║░██╔╝██║░░██║██║░░░██║██╔══██╗
+╚█████╗░░░░██║░░░███████║██║░░╚═╝█████═╝░███████║██║░░░██║██████╦╝
+░╚═══██╗░░░██║░░░██╔══██║██║░░██╗██╔═██╗░██╔══██║██║░░░██║██╔══██╗
+██████╔╝░░░██║░░░██║░░██║╚█████╔╝██║░╚██╗██║░░██║╚██████╔╝██████╦╝
+╚═════╝░░░░╚═╝░░░╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝░╚═════╝░╚═════╝░""")
+print("""
+    Select site option:
+1. English
+2. Russian
+""")
+user_option = input("Menu > ")
+if user_option == "English":
+    print("English module is loading")
+    pd.time.sleep(1)
+    print(pd.StackEng.OverEng())
+else:
+    print("Russian module is loading")
+    pd.time.sleep(1)
+    print(pd.StackRu.OverRu())
+#Create KailUser(☄ S͇͇y͇͇i͇͇r͇͇e͇͇z͇͇z͇͇ ☄#2023)
