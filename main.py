7dc072378aeb0723016d0691373e823149e25c8b
Colin <harebovmakar@yandex.ru>
2023-01-15 21:29:11 +0500

    Update 1.5v

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import __init__ as pd
+import os
 
 print("""
 ░██████╗████████╗░█████╗░░█████╗░██╗░░██╗██╗░░██╗██╗░░░██╗██████╗░
@@ -16,9 +17,11 @@ user_option = input("Menu > ")
 if user_option == "English":
     print("English module is loading")
     pd.time.sleep(1)
+    os.system("cls")
     print(pd.StackEng.OverEng())
 else:
     print("Russian module is loading")
     pd.time.sleep(1)
+    os.system("cls")
     print(pd.StackRu.OverRu())
 #Create KailUser(☄ S͇͇y͇͇i͇͇r͇͇e͇͇z͇͇z͇͇ ☄#2023)
